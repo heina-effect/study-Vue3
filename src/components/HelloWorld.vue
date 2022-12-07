033db378446125f0d6233fcba4fa4880f446590e
heina-effect <heina2ffect@gmail.com>
2022-12-07 18:20:26 +0900

    fix router index.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ToggleButton from '@/components/basic/ToggleButton.vue'
 defineProps({
   msg: {
     type: String,
@@ -15,6 +16,9 @@ defineProps({
       <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
+    <div>
+      <ToggleButton />
+    </div>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from 'vue-auto-routing'
-import { createRouterLayout } from 'vue-router-layout'
+import HelloWorld from '@/components/HelloWorld.vue'
 
-const RouterLayout = createRouterLayout(layout => {
-  return import('@/layouts/' + layout + '.vue')
-})
-
+const routes = [
+  {
+    path: '/',
+    component: HelloWorld,
+  }
+]
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: RouterLayout,
-      children: routes
-    }
-  ]
+  // history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
